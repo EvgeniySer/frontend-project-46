a36ebf5017beffa0d1efcd03bf39a22cee49a44a
Evgeniy Sergeev <serserser84@gmail.com>
2026-03-29 00:16:21 +0300

    task 2

--- gendiff.js
+++ gendiff.js
@@ -2,10 +2,18 @@
 
 import { program } from 'commander';
 
+const version = '1.0.0';
+
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'display help for command');
+  .version(version, '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'display help for command')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(`Comparing files:\nFile 1: ${filepath1}\nFile 2: ${filepath2}`);
+    console.log(`Format: ${program.opts().format || 'default'}`);
+  });
 
 program.parse();
