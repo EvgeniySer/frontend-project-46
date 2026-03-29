ac7e32117288d0523bbef31ed30b84e4612d5409
Evgeniy Sergeev <serserser84@gmail.com>
2026-03-29 18:55:22 +0300

    task 5 v3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -32,10 +32,10 @@ describe('gendiff', () => {
     const data = parse(filepath);
 
     const expected = `{
-  follow: false
-  host: hexlet.io
-  proxy: 123.234.53.22
-  timeout: 50
+      follow: false
+      host: hexlet.io
+      proxy: 123.234.53.22
+      timeout: 50
 }`;
 
 
