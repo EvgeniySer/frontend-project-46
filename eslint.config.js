2e183b723d6e4cc8dc05dca3fc7cd401f1a449cd
Evgeniy Sergeev <serserser84@gmail.com>
2026-03-30 23:18:20 +0300

    fighting errors and linters v2

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,35 @@
 // eslint.config.js
-import js from '@eslint/js';
 import globals from 'globals';
 
-export default [
-  {
-    ignores: [
-      '**/node_modules/**',
-      '**/coverage/**',
-      '**/dist/**',
-      '**/build/**',
-      '**/__tests__/**',
-      '**/*.test.js',
-      '**/*.spec.js',
-      '**.min.js',
-      '**/vendor/**'
-    ]
-  },
-  js.configs.recommended,
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    },
-    rules: {
-      '@stylistic/semi': 'off',
-      '@stylistic/indent': 'off',
-      '@stylistic/no-trailing-spaces': 'off',
-      '@stylistic/operator-linebreak': 'off',
-      '@stylistic/brace-style': 'off',
-      '@stylistic/comma-dangle': 'off',
-      '@stylistic/arrow-parens': 'off',
-      '@stylistic/max-statements-per-line': 'off',
-      '@stylistic/no-multiple-empty-lines': 'off'
+export default {
+  ignores: [
+    '**/node_modules/**',
+    '**/coverage/**',
+    '**/dist/**',
+    '**/build/**',
+    '**/__tests__/**',
+    '**/*.test.js',
+    '**/*.spec.js',
+    '**.min.js',
+    '**/vendor/**'
+  ],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    globals: {
+      ...globals.browser,
+      ...globals.node
     }
+  },
+  rules: {
+    '@stylistic/semi': 'off',
+    '@stylistic/indent': 'off',
+    '@stylistic/no-trailing-spaces': 'off',
+    '@stylistic/operator-linebreak': 'off',
+    '@stylistic/brace-style': 'off',
+    '@stylistic/comma-dangle': 'off',
+    '@stylistic/arrow-parens': 'off',
+    '@stylistic/max-statements-per-line': 'off',
+    '@stylistic/no-multiple-empty-lines': 'off'
   }
-];
+};
