cd51b11449d457ebb1a90af4c776f5c0d1d5dfe6
Evgeniy Sergeev <serserser84@gmail.com>
2026-03-31 00:01:38 +0300

    another linter fix v3

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,41 @@
+import js from '@eslint/js';
 import globals from 'globals';
 
-export default {
-  ignores: [
-    '**/node_modules/**',
-    '**/dist/**',
-    '**/build/**',
-    '**/__tests__/**',
-    '**/*.test.js',
-    '**/*.spec.js',
-    '**.min.js',
-    '**/vendor/**',
-    'eslint.config.js', 
-    '/project/code/coverage/**', 
-    'coverage/**'
-  ],
-  languageOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    globals: {
-      ...globals.browser,
-      ...globals.node
-    }
+export default [
+  {
+    ignores: [
+      '**/node_modules/**',
+      'coverage/**',
+      'dist/**',
+      'build/**',
+      '__tests__/**',
+      '**/*.test.js',
+      '**/*.spec.js',
+      '**.min.js',
+      'vendor/**'
+    ]
   },
-  rules: {
-    '@stylistic/semi': 'off',
-    '@stylistic/indent': 'off',
-    '@stylistic/no-trailing-spaces': 'off',
-    '@stylistic/operator-linebreak': 'off',
-    '@stylistic/brace-style': 'off',
-    '@stylistic/comma-dangle': 'off',
-    '@stylistic/arrow-parens': 'off',
-    '@stylistic/max-statements-per-line': 'off',
-    '@stylistic/no-multiple-empty-lines': 'off',
-    '@stylistic/eol-last': 'off'
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      '@stylistic/semi': 'off',
+      '@stylistic/indent': 'off',
+      '@stylistic/no-trailing-spaces': 'off',
+      '@stylistic/operator-linebreak': 'off',
+      '@stylistic/brace-style': 'off',
+      '@stylistic/comma-dangle': 'off',
+      '@stylistic/arrow-parens': 'off',
+      '@stylistic/max-statements-per-line': 'off',
+      '@stylistic/no-multiple-empty-lines': 'off',
+      '@stylistic/eol-last': 'off'
+    }
   }
-}
+];
